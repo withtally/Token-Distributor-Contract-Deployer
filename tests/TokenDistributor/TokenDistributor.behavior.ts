7f96de8da06e88bee48f9712ba62dc0f75a389bc
Arthur Abeilice <afa7789@gmail.com>
2023-11-29 16:00:04 -0300

    emit on test + other iteration to check pending designation.

--- tests/TokenDistributor/TokenDistributor.behavior.ts
+++ tests/TokenDistributor/TokenDistributor.behavior.ts
@@ -1,7 +1,11 @@
 import { expect } from "chai";
 import { ethers } from "ethers";
+// import json from files
+import json from '../../files/example_output_13800000000000000000000_0xe0411005ab8b1a495bd3804b43874ef67471fd63c00a73e817c5d6e42e551738.json';
+
 
 export function shouldBehaveLikeTD(): void {
+  
   it("should initialize the contract correctly", async function () {
     expect(await this.tokenDistributor.root()).to.equal(this.root);
     expect(await this.tokenDistributor.token()).to.equal(await this.token.getAddress());
@@ -16,6 +20,10 @@ export function shouldBehaveLikeTD(): void {
     );
   });
 
+  it("true is true"), async function () {
+    expect(true).to.equal(true);
+  }
+
   it("merkle tree verify works"), async function () {
 
 	// Account add to merkle tree
@@ -23,6 +31,7 @@ export function shouldBehaveLikeTD(): void {
 	// Private Key: 0x8166f546bab6da521a8369cab06c5d2b9e46670292d85c875ee9ec20e84ffb61
 	// Now you can use the `signer` otherAccount to interact with the Ethereum network
 	const privateKey = "0x8166f546bab6da521a8369cab06c5d2b9e46670292d85c875ee9ec20e84ffb61";
+  const pubKey = "0xcd3B766CCDd6AE721141F452C550Ca635964ce71";
 	const wallet = new ethers.Wallet(privateKey);
 	const otherAccount = wallet.connect(null); // Replace `provider` with your Ethereum provider
 	
@@ -34,10 +43,19 @@ export function shouldBehaveLikeTD(): void {
 	// function claim(bytes32[] calldata _proof, uint256 _amount) external {
 	// 	_claim(_proof, _amount);
 	// }
-    expect(await this.tokenDistributor.connect(otherAccount).claim(
-		proof,
-		amount,
-    )).to.equal(true);
+  //  emit Claimed({_user: msg.sender, _amount: _amount});
+  await expect(
+      this.tokenDistributor.connect(otherAccount).
+      claim(
+        json[pubKey].proofs,
+        json[pubKey].amount,
+      )
+    ).to.emit(this.tokenDistributor, 'Claimed').
+    withArgs(
+      otherAccount.address,
+      json['0xcd3B766CCDd6AE721141F452C550Ca635964ce71'].amount,
+    );
+
   }
 
 }
