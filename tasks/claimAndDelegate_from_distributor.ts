1ce034624c89cb3352c7cccfb569eb475b607c73
Arthur Abeilice <afa7789@gmail.com>
2023-12-01 14:22:10 -0300

    Adding tasks to be tested, in local node.

---
+++ tasks/claimAndDelegate_from_distributor.ts
@@ -0,0 +1,100 @@
+import { task } from "hardhat/config";
+import type { TaskArguments } from "hardhat/types";
+import {
+  MyERC20__factory,
+  TokenDistributor,
+  TokenDistributor__factory,
+} from "../types";
+import { signDelegateTransaction } from "../helpers/sign";
+
+task(
+  "claim_delegate",
+  "Claims and delegates tokens from a token distributor."
+)
+  .addParam(
+    "token_distributor_address",
+    "The address of the token distributor."
+  )
+  .addParam("json_path", "The path to the JSON file.")
+  .addOptionalParam(
+    "delegate",
+    "ethereum address who will be delegated to. e.g: 0x70997970C51812dc3A010C7d01b50e0d17dc79C8"
+  )
+  .setAction(async (taskArgs: TaskArguments, { ethers, network }) => {
+    console.log("Claiming and delegating tokens from the token distributor...");
+
+    const { token_distributor_address, json_path } = taskArgs;
+
+    // Load the JSON file
+    const json = require(json_path);
+
+    // Get the signer address
+    const signers = await ethers.getSigners();
+    const signer = signers[0];
+    const signerAddress = await signer.getAddress();
+
+    // get delegate address
+    const _delegateTo: string = taskArgs.delegate
+      ? taskArgs.delegate
+      : signer.address;
+
+    const expiry = Math.floor(Date.now() / 1000) + 60 * 60 * 24; // 24 hours
+
+    if (!json[signerAddress]) {
+      throw new Error(
+        `The signer address ${signerAddress} is not in the JSON file.`
+      );
+    }
+
+    // Find the amount and proof in the JSON using the signer address
+    const amount = json[signerAddress].amount;
+    const proofs = json[signerAddress].proof;
+
+    // Connect the contract using the token distributor address
+    const contract: TokenDistributor = await TokenDistributor__factory.connect(
+      token_distributor_address
+    );
+
+    // generate v,r,s
+    const tokenAddress = await contract.token();
+    // Connect the contract using the token address
+    const erc20 = await MyERC20__factory.connect(tokenAddress);
+
+    // nonce from signer
+    const nonce = await erc20.nonces(signerAddress);
+
+    // get chainID on hardhat
+    const chainId = network.config.chainId;
+    const signature = await signDelegateTransaction({
+      contractAddress: await erc20.getAddress(),
+      contractName: await erc20.name(),
+      delegateeAddress: signerAddress,
+      chainId: chainId ? chainId : 31337, // fetch from hardhat or use as hardhat local
+      nonce,
+      expiry,
+      signer: signer,
+    });
+
+    const { v, r, s } = ethers.Signature.from(signature);
+
+    // Make a call to the contract to claim and delegate tokens
+    const tx = await contract.claimAndDelegate(
+      proofs,
+      amount,
+      _delegateTo,
+      100000n,
+      v,
+      r,
+      s
+    );
+
+    // Wait for the transaction to be mined
+    const receipt = await tx.wait();
+
+    // Check Status
+    if (receipt?.status === 0) {
+      throw new Error("Transaction failed");
+    } else {
+      console.log("Tokens claimed and delegated successfully!");
+    }
+  });
---
+++ tasks/claim_from_distributor.ts
@@ -0,0 +1,50 @@
+import { task } from "hardhat/config";
+import type { TaskArguments } from "hardhat/types";
+import { TokenDistributor, TokenDistributor__factory } from "../types";
+
+task("claim", "Claims tokens from a token distributor.")
+  .addParam(
+    "token_distributor_address",
+    "The address of the token distributor."
+  )
+  .addParam("json_path", "The path to the JSON file.")
+  .setAction(async (taskArgs: TaskArguments, { ethers }) => {
+    console.log("Claiming tokens from the token distributor...");
+
+    const { token_distributor_address, json_path } = taskArgs;
+
+    // Load the JSON file
+    const json = require(json_path);
+
+    // Get the signer address
+    const signers = await ethers.getSigners();
+    const signerAddress = await signers[0].getAddress();
+
+    if (!json[signerAddress]) {
+      throw new Error(
+        `The signer address ${signerAddress} is not in the JSON file.`
+      );
+    }
+
+    // Find the amount and proof in the JSON using the signer address
+    const amount = json[signerAddress].amount;
+    const proofs = json[signerAddress].ss;
+
+    // Deploy the contract using the token distributor address
+    const contract: TokenDistributor = await TokenDistributor__factory.connect(
+      token_distributor_address
+    );
+
+    // Make a call to the contract to claim tokens
+    const tx = await contract.claim( proofs,amount);
+
+    // Wait for the transaction to be mined
+    const receipt = await tx.wait();
+
+    // Check Status
+    if (receipt?.status === 0) {
+      throw new Error("Transaction failed");
+    } else {
+      console.log("Tokens claimed successfully!");
+    }
+  });
